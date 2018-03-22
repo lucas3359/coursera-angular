2c43bf4303b70a92913a8c9f3aad8655a218494b
Lucas <lucashsw94@gmail.com>
2018-03-22 20:43:13 +1300

    Third Update.

---
+++ app.js
@@ -0,0 +1,13 @@
+(function(){
+'use strict';
+
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', function($scope){
+  $scope.name = "Lucas";
+  $scope.sayHellow = function(){
+    return "Hellop Coursera";
+  };
+});
+
+})();
